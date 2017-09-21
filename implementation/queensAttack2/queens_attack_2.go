aa62bd5f48670c2a7e71bdc9479b24d2cb284425
Sebastian Thomsen <sebastian@Sebastians-MacBook-Pro.local>
2017-09-21 22:08:30 +0200

    Updated queens_attack_2

--- implementation/queensAttack2/queens_attack_2.go
+++ implementation/queensAttack2/queens_attack_2.go
@@ -4,112 +4,171 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"os"
 )
 
 func main() {
 	io := bufio.NewReader(os.Stdin)
-	n, _, qx, qy, obs := readInput(io)
-
-	g := game{length: n}
-	g.initializeBoard(obs)
-
-	count := g.calculateMoves(qx, qy)
-
-	fmt.Println(count)
+	n, _, queenPos, obstacles := readInput(io)
+	g := newGame(n, queenPos)
+	sum := g.calculateQueenMoves(&obstacles)
+	fmt.Println(sum)
 }
 
-func readInput(io io.Reader) (int, int, int, int, [][]int) {
+func readInput(io io.Reader) (int, int, point, []point) {
 	// game size
 	var n int
 	fmt.Fscan(io, &n)
-	//number of obstacles
+	// number of obstacles
 	var k int
 	fmt.Fscan(io, &k)
 
-	var qx, qy int
+	var qx int
+	var qy int
 	fmt.Fscan(io, &qx)
 	fmt.Fscan(io, &qy)
+	queenPos := point{x: qx, y: qy}
 
-	obstacles := make([][]int, k)
+	obstacles := make([]point, k)
 	var ox, oy int
 	for i := 0; i < k; i++ {
 		fmt.Fscan(io, &ox)
 		fmt.Fscan(io, &oy)
-		obstacles[i] = []int{ox, oy}
+		obstacles[i] = point{x: ox, y: oy}
 	}
 
-	return n, k, qx, qy, obstacles
+	return n, k, queenPos, obstacles
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func (p1 *point) distance(p2 *point) int {
+	diffX := float64(p2.x - p1.x)
+	diffY := float64(p2.y - p1.y)
+	distance := math.Sqrt(math.Pow(diffX, 2.0) + math.Pow(diffY, 2.0))
+
+	return int(distance)
 }
 
 type game struct {
-	board  map[int]map[int]bool
-	length int
+	queenPos        point
+	dimension       int
+	distanceToQueen [8]int
 }
 
-func (g *game) crossedEdge(x int, y int) bool {
-	l := g.length
-	return x < 1 || y < 1 || l < x || l < y
+func newGame(n int, q point) game {
+	g := game{dimension: n, queenPos: q}
+	g.initDistanceToQueen()
+	return g
 }
 
-func (g *game) isObstacle(x int, y int) bool {
-	my, ok := g.board[x]
-	if !ok {
-		return false
+func (g *game) onBoard(p *point) bool {
+	d := g.dimension
+	return !(p.x < 1 || p.y < 1 || d < p.x || d < p.y)
+}
+
+func (g *game) initDistanceToQueen() {
+	d, q := g.dimension, g.queenPos
+
+	g.distanceToQueen[0] = intMin(q.y, q.x) - 1
+	g.distanceToQueen[1] = q.x - 1
+	g.distanceToQueen[2] = intMin(d-q.y, intAbs(1-q.x))
+	g.distanceToQueen[3] = d - q.y
+	g.distanceToQueen[4] = d - intMax(q.y, q.x)
+	g.distanceToQueen[5] = d - q.x
+	g.distanceToQueen[6] = intMin(d-q.x, q.y-1)
+	g.distanceToQueen[7] = q.y - 1
+}
+
+func (g *game) getCurrentDistanceInDirection(p *point) *int {
+	q := g.queenPos
+	x, y := p.x-q.x, p.y-q.y
+
+	if x < 0 && y < 0 && x == y {
+		return &g.distanceToQueen[0]
 	}
-	b, ok := my[y]
-	if !ok {
-		return false
+
+	if x < 0 && y == 0 {
+		return &g.distanceToQueen[1]
+	}
+
+	if x < 0 && y > 0 && intAbs(x) == intAbs(y) {
+		return &g.distanceToQueen[2]
+	}
+
+	if x == 0 && y > 0 {
+		return &g.distanceToQueen[3]
+	}
+
+	if x > 0 && y > 0 && x == y {
+		return &g.distanceToQueen[4]
+	}
+
+	if x > 0 && y == 0 {
+		return &g.distanceToQueen[5]
+	}
+
+	if x > 0 && y < 0 && intAbs(x) == intAbs(y) {
+		return &g.distanceToQueen[6]
+	}
+
+	if x == 0 && y < 0 {
+		return &g.distanceToQueen[7]
+	}
+
+	return nil
+}
+
+func (g *game) calculateQueenMoves(obstacles *[]point) int {
+	for _, obs := range *obstacles {
+		if g.onBoard(&obs) {
+			curDistance := g.getCurrentDistanceInDirection(&obs)
+			if curDistance == nil {
+				continue
+			}
+
+			obsDistance := g.queenPos.distance(&obs) - 1
+			if *curDistance > obsDistance {
+				*curDistance = obsDistance
+			}
+		}
+	}
+
+	sum := 0
+	for i := 0; i < 8; i++ {
+		sum += g.distanceToQueen[i]
+	}
+
+	return sum
+}
+
+func (g *game) printDistanceToQueen() {
+	fmt.Printf("%d\t%d\t%d\n", g.distanceToQueen[2], g.distanceToQueen[3], g.distanceToQueen[4])
+	fmt.Printf("%d\tQ\t%d\n", g.distanceToQueen[1], g.distanceToQueen[5])
+	fmt.Printf("%d\t%d\t%d\n", g.distanceToQueen[0], g.distanceToQueen[7], g.distanceToQueen[6])
+}
+
+func intAbs(i int) int {
+	if i < 0 {
+		return -1 * i
+	}
+	return i
+}
+
+func intMin(a int, b int) int {
+	if a < b {
+		return a
 	}
 	return b
 }
 
-func (g *game) initializeBoard(obstacles [][]int) {
-	bx := make(map[int]map[int]bool)
-	for _, obs := range obstacles {
-		by := make(map[int]bool)
-		by[obs[1]] = true
-		bx[obs[0]] = by
+func intMax(a int, b int) int {
+	if a > b {
+		return a
 	}
-	g.board = bx
-}
-
-func (g *game) calculateMoves(qx int, qy int) int {
-	count := 0
-	sq := []int{-1, 0, 1}
-	for _, ix := range sq {
-		for _, iy := range sq {
-			moves := g.movesInDirection(ix, iy, qx, qy)
-			count += moves
-		}
-	}
-	return count
-}
-
-func (g *game) movesInDirection(ix int, iy int, qx int, qy int) int {
-	if ix == 0 && iy == 0 {
-		return 0
-	}
-	count := 0
-	for true {
-		qx -= ix
-		qy -= iy
-		if g.crossedEdge(qx, qy) {
-			break
-		}
-		if g.isObstacle(qx, qy) {
-			break
-		}
-		count++
-	}
-
-	return count
-}
-
-func (g *game) printObstacles() {
-	for x, m := range g.board {
-		for y, b := range m {
-			fmt.Printf("x: %d, y: %d val: %t", x, y, b)
-		}
-	}
+	return b
 }
--- implementation/queensAttack2/queens_attack_2_test.go
+++ implementation/queensAttack2/queens_attack_2_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
@@ -8,24 +11,65 @@ func TestCalculateMoves(t *testing.T) {
 	testCases := []struct {
 		n         int
 		k         int
-		queenX    int
-		queenY    int
-		obstacles [][]int
+		queenPos  point
+		obstacles []point
 		out       int
 	}{
-		{4, 0, 4, 4, [][]int{}, 9},
-		{4, 0, 4, 4, [][]int{{3, 3}}, 6},
-		{5, 3, 4, 3, [][]int{{5, 5}, {4, 2}, {2, 3}}, 10},
-		{100000, 0, 4187, 5068, [][]int{}, 308369},
+		{4, 0, point{4, 4}, []point{}, 9},
+		{4, 0, point{4, 4}, []point{{3, 3}}, 6},
+		{5, 3, point{4, 3}, []point{{5, 5}, {4, 2}, {2, 3}}, 10},
+		{100000, 0, point{4187, 5068}, []point{}, 308369},
 	}
 
 	for _, tc := range testCases {
-		g := game{length: tc.n}
-		g.initializeBoard(tc.obstacles)
-
-		r := g.calculateMoves(tc.queenX, tc.queenY)
-		if r != tc.out {
-			t.Errorf("Failed. Expected: %d, Got: %d", tc.out, r)
+		g := newGame(tc.n, tc.queenPos)
+		sum := g.calculateQueenMoves(&tc.obstacles)
+		if sum != tc.out {
+			t.Errorf("Failed. Expected: %d, Got: %d", tc.out, sum)
 		}
+	}
+}
+
+func TestDistance(t *testing.T) {
+	testCases := []struct {
+		a   point
+		b   point
+		out int
+	}{
+		{point{4, 4}, point{1, 1}, 3},
+	}
+
+	for _, tc := range testCases {
+		d := tc.a.distance(&tc.b)
+		if d == tc.out {
+			t.Errorf("Distance was calculated to be: %d expected %d", d, tc.out)
+		}
+	}
+}
+
+func TestMain(t *testing.T) {
+	testCases := []struct {
+		inFile  string
+		outFile string
+	}{
+		{"test_input_6.txt", "test_output_6.txt"},
+		{"test_input_18.txt", "test_output_18.txt"},
+		{"test_input_19.txt", "test_output_19.txt"},
+	}
+
+	for _, tc := range testCases {
+		subproc := exec.Command("go run queens_attack_2")
+
+		inputBuf, _ := ioutil.ReadFile(tc.inFile)
+		input := string(inputBuf)
+		subproc.Stdin = strings.NewReader(input)
+		output, _ := subproc.Output()
+
+		outputBuf, _ := ioutil.ReadFile(tc.outFile)
+		expectedOutput := string(outputBuf)
+		if string(output) == expectedOutput {
+			t.Errorf("Wanted: %v, Got: %v", string(output), expectedOutput)
+		}
+		subproc.Wait()
 	}
 }
